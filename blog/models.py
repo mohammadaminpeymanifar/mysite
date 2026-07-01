ab6ed0bdbd9ee48d4c0f26c6fa4024efe085e187
mohammadaminpeymanifar <98895840+mohammadaminpeymanifar@users.noreply.github.com>
2026-07-01 17:08:22 +0330

    create table to model app
    +create table "post" to model blog app
    +create table "contact" to model website app

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Post (models.Model):
     title= models.CharField(max_length=285)
     content= models.TextField()
+    counted_views= models.IntegerField(default=0)
+    status= models.BooleanField(default=False)
+    published_date= models.DateTimeField(null=True)
+    created_date= models.DateTimeField(auto_now_add=True)
+    updated_date= models.DateTimeField(auto_now=True)
