939c7a01ec06ecaf39e17a35169c9e144a90a9ad
mohammadaminpeymanifar <98895840+mohammadaminpeymanifar@users.noreply.github.com>
2026-07-05 02:37:49 +0330

    edit app blog
    edit blog urls and views

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from blog.models import Post
 # Create your views here.
 def blog_view(request):
+    posts = Post.objects.filter(status=1)
+    context = {'posts':posts}
     return render(request , 'blog/blog-home.html')
 
 def blog_single(request):
     return render(request , 'blog/blog-single.html')
+
+def test(request):
+    posts = Post.objects.all()
+    context = {'posts':posts}
+    return render(request , 'test.html',context)
