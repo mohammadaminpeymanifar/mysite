9685593d42be00c9eeb8ae9bc377a11e129d9afa
mohammadaminpeymanifar <98895840+mohammadaminpeymanifar@users.noreply.github.com>
2026-07-12 19:59:32 +0330

    work in Module Django
    +Humanize
    +extension
    +Sites framework
    +Sitemap framework
    +Robots
    +Debug ToolBar
    +Taggit

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,14 +31,23 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #App 
     'website.apps.WebsiteConfig',
     'blog.apps.BlogConfig',
+    #Default Mudule
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    #Mudule Install
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
+    'debug_toolbar',
+    'django_extensions',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -132,3 +142,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
