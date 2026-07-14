f0fcebc232e3ae51b3b7187fa10e63a12689051c
mohammadaminpeymanifar <98895840+mohammadaminpeymanifar@users.noreply.github.com>
2026-07-14 23:57:24 +0330

    Login form
    add template to Login page

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 
 def login_view(request):
-    if request.user.is_authenticated:
-        message = f'user is authenticated as {request.user.username}'
-    else:
-        message = 'user is not authenticated'
-    return render (request,'accounts/login.html',{'message':message})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+        
+    return render(request,'accounts/login.html')
 
 
 # def logout_view(request):
